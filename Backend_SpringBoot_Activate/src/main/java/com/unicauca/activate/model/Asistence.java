c877fb6586ad002a057f78684e152afdf45cdadd
s3rodriguez10 <s3rodriguez10@gmail.com>
2022-02-01 16:37:11 -0500

    Add-Asistence-Class

---
+++ Backend_SpringBoot_Activate/src/main/java/com/unicauca/activate/model/Asistence.java
@@ -0,0 +1,27 @@
+package com.unicauca.activate.model;
+
+public class Asistence {
+    private Long identificacionUsuario;
+    private Long identificacionEvento;
+
+    
+
+    public Asistence(Long identificacionUsuario, Long identificacionEvento) {
+        this.identificacionUsuario = identificacionUsuario;
+        this.identificacionEvento = identificacionEvento;
+    }
+    public Long getIdentificacionUsuario() {
+        return identificacionUsuario;
+    }
+    public void setIdentificacionUsuario(Long identificacionUsuario) {
+        this.identificacionUsuario = identificacionUsuario;
+    }
+    public Long getIdentificacionEvento() {
+        return identificacionEvento;
+    }
+    public void setIdentificacionEvento(Long identificacionEvento) {
+        this.identificacionEvento = identificacionEvento;
+    }
+    
+    
+}
