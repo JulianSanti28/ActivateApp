41da3f2da65d535d44050b9c08cd3a0546d199a6
JulianSanti28 <juliansmartinez@unicauca.edu.co>
2022-02-24 14:54:49 -0500

    Update LoginController class

--- Backend_SpringBoot_Activate/src/main/java/com/unicauca/activate/controller/LoginController.java
+++ Backend_SpringBoot_Activate/src/main/java/com/unicauca/activate/controller/LoginController.java
@@ -26,7 +26,6 @@ public class LoginController {
     //Crear Usuario
     @PostMapping("login")
     public String  login(@RequestBody User user){
-        System.out.println("Desde JS"+ " " + user.getEmail());
         User usuarioLogueado = UserService.verificarCredenciales(user);
         if (usuarioLogueado != null) {
             String tokenJwt = jwtUtil.create(String.valueOf(usuarioLogueado.getId()), usuarioLogueado.getEmail());
