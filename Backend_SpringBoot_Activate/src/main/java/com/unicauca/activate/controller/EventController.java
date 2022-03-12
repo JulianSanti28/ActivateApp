f720b1e773edb7934bf45c72e697ed9ac1f82283
s3rodriguez10 <s3rodriguez10@gmail.com>
2022-03-11 20:13:53 -0500

    PUT request solution

--- Backend_SpringBoot_Activate/src/main/java/com/unicauca/activate/controller/EventController.java
+++ Backend_SpringBoot_Activate/src/main/java/com/unicauca/activate/controller/EventController.java
@@ -32,6 +32,7 @@ import org.hibernate.annotations.SourceType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,6 +41,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -54,7 +56,10 @@ import ch.qos.logback.classic.Logger;
  */
 @RestController
 @RequestMapping("/activate/event")
+@CrossOrigin(origins = "*", methods= {RequestMethod.GET,RequestMethod.PUT})
 public class EventController {
+
+    
 
     private Mapper mapper = new Mapper();
 
