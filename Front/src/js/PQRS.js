b71cbc87defc1c072a1861396c4937db6ab2c312
miguemm <mandresmosquera@gmail.com>
2022-03-14 09:28:04 -0500

    Se añadio PQRS

---
+++ Front/src/js/PQRS.js
@@ -0,0 +1,45 @@
+$(document).ready(function () {
+
+    //Pendiente al envio del formulario
+    const form = document.getElementById("PQRSForm");
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+        registrarPQRS();       
+    })
+});
+
+function getHeaders() {
+    return {
+     'Accept': 'application/json',
+     'Content-Type': 'application/json',
+     'Authorization': localStorage.token
+   };
+}
+
+async function registrarPQRS(){
+    let datos = {};
+
+    var select_request_type = document.getElementById("select_request_type"); /*Obtener el SELECT de tipo de Request*/
+    datos.tipo = select_request_type.options[select_request_type.selectedIndex].value; /*Obtener id de la opción destino*/
+    datos.descripcion = document.getElementById('compose-textarea').value;
+    datos.identificacionEvento = localStorage.verEvento;
+
+
+    // const request = await fetch('http://localhost:8081/activate/event/PQRS', {
+    //     method: 'POST',
+    //     headers: getHeaders(),
+    //     body: JSON.stringify(datos)
+    // }).then(response => response.json())
+    // .then(result => {
+        
+
+    // });
+
+    const contenedor_pqrs = document.getElementById("contenedor_pqrs");
+    contenedor_pqrs.style.display = 'none';
+
+    const contenedor_response = document.getElementById("contenedor_response");
+    contenedor_response.style.display = 'block';
+
+    document.getElementById("request_responsable").innerHTML = "Su madre";
+}
